70d3c92e8a44147123745595fbb85af4b6f159e9
Jonatan Flores <ti02@alfasistemas.inf.br>
2017-12-14 16:32:53 -0200

    Added support for CORS and also removed function session_start() from index.php file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -45,6 +45,21 @@ $app = new \Slim\App([
 ]);
 
 /**
+ * Enabled CORS
+ */
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
+/**
  * Get an instance of the container object
  */
 $container = $app->getContainer();
